3699f31da0ce43bed303db6c0b31eeaf8359ead3
Semere <se.semere.talegngeta@gmail.com>
2021-07-04 23:01:15 +0300

    structuring server folder

--- config/index.js
+++ config/index.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  PORT: process.env.PORT,
-  MONGO_URI: process.env.MONGO_URI,
-  MONGO_DB_NAME: process.env.MONGO_DB_NAME,
+  PORT: process.env.PORT || 5000,
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017',
+  MONGO_DB_NAME: process.env.MONGO_DB_NAME || 'gron-verden',
   JWT_SECRET: process.env.JWT_SECRET
 };
